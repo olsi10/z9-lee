da390cd3f56c132cd326e0159f2144e4dee1784b
olsi10 <wangbampang2035@gmail.com>
2023-07-13 00:36:17 +0900

    필요한 패키지 다운 및 노드 + 오라클 연결 테스트

--- server/config/key.js
+++ server/config/key.js
@@ -1,1 +1,8 @@
 // db에서 받은 개인 키, gitingnore에 추가해서 배포X
+
+module.exports = {
+  user: "ZGOOLEE",
+  password: "1234",
+  connectString: "localhost:1521/xe",
+  externalAuth: false,
+};
--- server/index.js
+++ server/index.js
@@ -1,1 +1,53 @@
-// 서버 관리
+const express = require("express");
+const app = express();
+const dotenv = require("dotenv"); //.env 파일에 접근 가능한 모듈
+dotenv.config();
+
+const oracledb = require("oracledb");
+const dbConfig = require("./dbconfig.js");
+
+app.listen(3000, () => {
+  console.log("Start Sever : localhost:3000");
+});
+
+app.get("/", function (req, res) {
+  res.render("index.html");
+});
+
+app.get("/db", function (req, res) {
+  // DB Select
+  selectDatabase();
+
+  // 화면에 보여줄 txt
+  res.send("연결이다!!!!!!!");
+});
+
+// DB Select
+async function selectDatabase() {
+  console.log("!!!!! db conenction !!!!!");
+  console.log(dbConfig.user);
+  console.log(dbConfig.password);
+
+  let connection = await oracledb.getConnection(dbConfig);
+
+  let binds = {};
+  let options = {
+    outFormat: oracledb.OUT_FORMAT_OBJECT, // query result format
+  };
+
+  console.log("!!!!! db select !!!!!");
+
+  let result = await connection.execute("select * from POST2", binds, options);
+
+  console.log("!!!!! db response !!!!!");
+  console.log(result.rows[0]);
+
+  console.log("!!!!! db close !!!!!");
+  await connection.close();
+}
+
+function init() {
+  oracledb.initOracleClient({ libDir: "" });
+}
+
+init();
